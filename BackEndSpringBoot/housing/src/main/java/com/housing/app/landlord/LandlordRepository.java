54e806b344d083925cd57c3a3741f9408732251b
Yogue1997 <yousyogue@gmail.com>
2022-03-10 00:21:16 -0500

    completed

---
+++ BackEndSpringBoot/housing/src/main/java/com/housing/app/landlord/LandlordRepository.java
@@ -0,0 +1,17 @@
+package com.housing.app.landlord;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//import antlr.collections.List;
+
+import java.util.List;
+
+//import model.landlord;
+
+
+public interface LandlordRepository extends JpaRepository<Landlord, Integer> {
+
+	Landlord findByAddress(String address);
+	
+
+}
